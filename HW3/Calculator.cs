c52e5423ee3aac36bc451740bd5a3451844bca50
RahevinSlade <rpotterclark15@mail.wou.edu>
2017-10-21 13:56:51 -0700

    beginings of HW3

---
+++ HW3/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyProgram
+{
+    static class Program
+        [STAThread]
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("ABC");
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new Form1(args));
+    }
+}
+}
+
