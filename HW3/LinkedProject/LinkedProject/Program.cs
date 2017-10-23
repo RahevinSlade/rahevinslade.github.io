70a5b94e5146f7283c205956617393da38db12fc
RahevinSlade <rpotterclark15@mail.wou.edu>
2017-10-22 17:57:32 -0700

    I have Node idea whats going on, taking a break

--- HW3/LinkedProject/LinkedProject/Program.cs
+++ HW3/LinkedProject/LinkedProject/Program.cs
@@ -6,56 +6,17 @@ using System.Threading.Tasks;
 
 namespace LinkedProject
 {
-    public class Stack<T>
+    interface IFace
     {
-        public bool IsEmpty { get { return first == null; } }
-        private Node<T> first;
-
-        public void Push(T obj)
-        {
-            if (first == null)
-                first = new Node<T>(obj);
-            else
-            {
-                first = new Node<T>(obj, first);
-            }
-        }
-
-        public T Pop()
-        {
-            if (IsEmpty)
-                throw new Exception("Stack is empty");
-            else
-            {
-                T ell = first.Element;
-                first = first.Next;
-                return ell;
-            }
-        }
-
-        public T Peek()
-        {
-            if (IsEmpty)
-                throw new Exception("Stack is empty");
-            else
-                return first.Element;
-        }
-
-        private class Node<E>
-        {
-            public E Element { get; private set; }
-            public Node<E> Next { get; set; }
-
-            public Node(E element)
-            {
-                Element = element;
-            }
-
-            public Node(E element, Node<E> next)
-                : this(element)
-            {
-                Next = next;
-            }
-        }
+        Object push(Object newItem);
+        Object pop();
+        Object peek();
+        bool isEmpty();
+        void clear();
     }
+    public class LinkedStack
+    {
+
+    }
+   
 }
