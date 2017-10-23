a8da2ec1dc61950fc0f29d7bc35e865e375e2057
RahevinSlade <rpotterclark15@mail.wou.edu>
2017-10-22 17:21:24 -0700

    First commit of the Nodes, little Nodelings

---
+++ HW3/LinkedProject/LinkedProject/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Node
+{ 
+    public object data;
+    public Node next;
+
+	public Node()//this is the creation of the node
+	{
+    data = null;//carries but two item, the information, and whats next.
+    next = null;
+ 	}
+
+public Node(Object data, Node next)
+{
+    this.data = data;//this is the public node for all
+    this.next = next;
+}
+}
---
+++ HW3/LinkedProject/LinkedProject/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedProject
+{
+    public class Stack<T>
+    {
+        public bool IsEmpty { get { return first == null; } }
+        private Node<T> first;
+
+        public void Push(T obj)
+        {
+            if (first == null)
+                first = new Node<T>(obj);
+            else
+            {
+                first = new Node<T>(obj, first);
+            }
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty)
+                throw new Exception("Stack is empty");
+            else
+            {
+                T ell = first.Element;
+                first = first.Next;
+                return ell;
+            }
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+                throw new Exception("Stack is empty");
+            else
+                return first.Element;
+        }
+
+        private class Node<E>
+        {
+            public E Element { get; private set; }
+            public Node<E> Next { get; set; }
+
+            public Node(E element)
+            {
+                Element = element;
+            }
+
+            public Node(E element, Node<E> next)
+                : this(element)
+            {
+                Next = next;
+            }
+        }
+    }
+}
