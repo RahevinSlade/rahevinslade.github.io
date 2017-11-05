c183ed98877086aa88055bfd73d1bc5e4732e2a1
RahevinSlade <rpotterclark15@mail.wou.edu>
2017-11-04 17:47:33 -0700

    alright, here is part 1 and 2 done, I believe anyway

--- HW4/Forms/Forms/Controllers/HomeController.cs
+++ HW4/Forms/Forms/Controllers/HomeController.cs
@@ -16,18 +16,66 @@ namespace Forms.Controllers
             return View();
         }
 
-        public ActionResult Form1()
+        [HttpGet]
+        public ActionResult Convert1()
         {
-
-            string Fname = Request.Form["Fname"];
-            string Lname = Request.Form["Lname"];
-            Debug.WriteLine($"Hello, {Fname} {Lname}");
-
-            ViewBag.Lname = Lname;
-            ViewBag.Fname = Fname;
-
-            
             return View();
         }
+
+        [HttpPost]
+        [ActionName("Convert1")]
+        public ActionResult Page1()
+        {
+            int nume = Int32.Parse(Request.Form["nume"]);
+            int denom = Int32.Parse(Request.Form["denom"]);
+            string act = Request.Form["act"];
+
+            decimal result;
+
+            if(act == "P" || act == "p")
+            {
+                result = ((decimal)nume / denom) * 100;
+            }
+            
+            else if(act == "D" || act == "d")
+            {
+                result = (decimal)nume / denom;
+            }
+
+            else
+            {
+                return Content($"It appears you entered neither a P or a D, so we dont know what you want?");
+
+            }
+
+            return Content($"{result} Ta-Da, here is your result");
+        }
+
+       [HttpGet]//this is part two
+        public ActionResult Form1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Form1(FormCollection form)
+        {
+            string fname = Request.Form["fname"];
+            int num = Int32.Parse(Request.Form["num"]);
+            string gt;
+            if (fname.Length > 3)
+            {
+                gt = fname.Substring(0, 3) + num.ToString();
+            }
+            else
+            {
+                gt = fname + num.ToString();
+            }
+
+            ViewBag.newgt = ($"{gt}, is your new gamer tag and my I say, it looks Fabulous!"); 
+
+            return View();
+        }
+
     }
 }
