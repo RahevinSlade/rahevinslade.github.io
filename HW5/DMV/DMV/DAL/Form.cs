df916ff09a391dab26c036b4f9c661866d315887
RahevinSlade <rpotterclark15@mail.wou.edu>
2017-11-05 22:38:23 -0800

    this might be going somewhere, but I'm really really tired, so I left off at finishing my DAL

---
+++ HW5/DMV/DMV/DAL/Form.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace DMV.DAL
+{
+    public class Form : DMVadd
+    {
+        public Form() : base("name=Form")
+        {
+
+        }
+        public virtual DbSet<Change> Requests { get; set; }
+    }
+}
---
+++ HW5/DMV/DMV/Models/Change.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace DMV.Models
+{
+    public class Change
+    {
+        [Display(Name = "Customer Number"), Required]
+        public int ID { get; set; }
+
+        [Display(Name = "Date of birth:"), DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}"), Required]
+        public DateTime DOB { get; set; }
+
+        [Display(Name = "Name:"), Required]
+        public string Fullname { get; set; }
+
+        [Display(Name = "Address:"), Required]
+        public string address { get; set; }
+
+        [Display(Name = "CSZ:"), Required]
+        public string CSZ { get; set; }
+
+        [Display(Name = "County:"), Required]
+        public string County { get; set; }
+
+        [Display(Name = "Date:"), DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}"), Required]
+        public DateTime cdate { get; set; }
+
+    }
+}
