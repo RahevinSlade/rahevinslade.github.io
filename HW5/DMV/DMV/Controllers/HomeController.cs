0fbd427fa9183c53e8fd296ecb7821738dc9e88d
RahevinSlade <rpotterclark15@mail.wou.edu>
2017-11-05 19:09:20 -0800

    hooked up the database to the project

--- HW5/DMV/DMV/Controllers/HomeController.cs
+++ HW5/DMV/DMV/Controllers/HomeController.cs
@@ -13,18 +13,10 @@ namespace DMV.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Request()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
