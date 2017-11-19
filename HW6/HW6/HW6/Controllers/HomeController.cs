f46809ef9060a44187dc8e8dce5bb9d730c66010
RahevinSlade <rpotterclark15@mail.wou.edu>
2017-11-18 20:03:12 -0800

    tryna add the database here for adventure works

--- HW6/HW6/HW6/Controllers/HomeController.cs
+++ HW6/HW6/HW6/Controllers/HomeController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HW6.Models;
+using System.Diagnostics;
+
 
 namespace HW6.Controllers
 {
     public class HomeController : Controller
     {
+        HW6Context db = new HW6Context();
+
         public ActionResult Index()
         {
             return View();
---
+++ HW6/HW6/HW6/Models/HW6Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HW6.Models
+{
+    public partial class HW6Context : DbContext
+    {
+        public HW6Context()
+        {
+            : base("name=HW6Context")
+        }
+
+    }
+}
