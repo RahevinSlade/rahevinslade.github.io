f272b34ae2fd75f92d47c0c69c7a91e9e67f4c78
RahevinSlade <rpotterclark15@mail.wou.edu>
2017-11-21 22:19:32 -0800

    Working on trying to implement the models and I guess my db stopped working, soo thats gotta get fixed

---
+++ HW6/HW6/HW6/App_Data/Pull.sql
@@ -0,0 +1,4 @@
+﻿RESTORE DATABASE AdventureWorks2014  
+FROM DISK = 'C:\Users\Rahevin\Documents\CurrentClasses\CS 460\rahevinslade.github.io\HW6\AdventureWorks2014.bak'  
+WITH MOVE 'AdventureWorks2014_Data' TO 'C:\Users\Rahevin\Documents\CurrentClasses\CS 460\rahevinslade.github.io\HW6\AdventureWorks2014_Data.mdf',  
+MOVE 'AdventureWorks2014_Log' TO 'C:\Users\Rahevin\Documents\CurrentClasses\CS 460\rahevinslade.github.io\HW6\AdventureWorks2014_Log.ldf' 
--- HW6/HW6/HW6/Models/HW6Context.cs
+++ HW6/HW6/HW6/Models/HW6Context.cs
@@ -1,17 +1,257 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
+namespace HW6.Models
+
+{
+
+using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace HW6.Models
-{
     public partial class HW6Context : DbContext
     {
         public HW6Context()
-        {
             : base("name=HW6Context")
+        {
         }
+        public virtual DbSet<BillOfMaterial> BillOfMaterials { get; set; }
+        public virtual DbSet<Culture> Cultures { get; set; }
+        public virtual DbSet<Illustration> Illustrations { get; set; }
+        public virtual DbSet<Location> Locations { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductCategory> ProductCategories { get; set; }
+        public virtual DbSet<ProductCostHistory> ProductCostHistories { get; set; }
+        public virtual DbSet<ProductDescription> ProductDescriptions { get; set; }
+        public virtual DbSet<ProductInventory> ProductInventories { get; set; }
+        public virtual DbSet<ProductListPriceHistory> ProductListPriceHistories { get; set; }
+        public virtual DbSet<ProductModel> ProductModels { get; set; }
+        public virtual DbSet<ProductModelIllustration> ProductModelIllustrations { get; set; }
+        public virtual DbSet<ProductModelProductDescriptionCulture> ProductModelProductDescriptionCultures { get; set; }
+        public virtual DbSet<ProductPhoto> ProductPhotoes { get; set; }
+        public virtual DbSet<ProductProductPhoto> ProductProductPhotoes { get; set; }
+        public virtual DbSet<ProductReview> ProductReviews { get; set; }
+        public virtual DbSet<ProductSubcategory> ProductSubcategories { get; set; }
+        public virtual DbSet<ScrapReason> ScrapReasons { get; set; }
+        public virtual DbSet<TransactionHistory> TransactionHistories { get; set; }
+        public virtual DbSet<TransactionHistoryArchive> TransactionHistoryArchives { get; set; }
+        public virtual DbSet<UnitMeasure> UnitMeasures { get; set; }
+        public virtual DbSet<WorkOrder> WorkOrders { get; set; }
+        public virtual DbSet<WorkOrderRouting> WorkOrderRoutings { get; set; }
+        public virtual DbSet<ProductDocument> ProductDocuments { get; set; }
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BillOfMaterial>()
+                .Property(e => e.UnitMeasureCode)
+                .IsFixedLength();
+
+            modelBuilder.Entity<BillOfMaterial>()
+                .Property(e => e.PerAssemblyQty)
+                .HasPrecision(8, 2);
+
+            modelBuilder.Entity<Culture>()
+                .Property(e => e.CultureID)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Culture>()
+                .HasMany(e => e.ProductModelProductDescriptionCultures)
+                .WithRequired(e => e.Culture)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Illustration>()
+                .HasMany(e => e.ProductModelIllustrations)
+                .WithRequired(e => e.Illustration)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Location>()
+                .Property(e => e.CostRate)
+                .HasPrecision(10, 4);
+
+            modelBuilder.Entity<Location>()
+                .Property(e => e.Availability)
+                .HasPrecision(8, 2);
+
+            modelBuilder.Entity<Location>()
+                .HasMany(e => e.ProductInventories)
+                .WithRequired(e => e.Location)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Location>()
+                .HasMany(e => e.WorkOrderRoutings)
+                .WithRequired(e => e.Location)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.StandardCost)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.ListPrice)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.SizeUnitMeasureCode)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.WeightUnitMeasureCode)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.Weight)
+                .HasPrecision(8, 2);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.ProductLine)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.Class)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.Style)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Product>()
+                .HasMany(e => e.BillOfMaterials)
+                .WithRequired(e => e.Product)
+                .HasForeignKey(e => e.ComponentID)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(e => e.BillOfMaterials1)
+                .WithOptional(e => e.Product1)
+                .HasForeignKey(e => e.ProductAssemblyID);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(e => e.ProductCostHistories)
+                .WithRequired(e => e.Product)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Product>()
+                .HasOptional(e => e.ProductDocument)
+                .WithRequired(e => e.Product);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(e => e.ProductInventories)
+                .WithRequired(e => e.Product)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(e => e.ProductListPriceHistories)
+                .WithRequired(e => e.Product)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(e => e.ProductProductPhotoes)
+                .WithRequired(e => e.Product)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(e => e.ProductReviews)
+                .WithRequired(e => e.Product)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(e => e.TransactionHistories)
+                .WithRequired(e => e.Product)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(e => e.WorkOrders)
+                .WithRequired(e => e.Product)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ProductCategory>()
+                .HasMany(e => e.ProductSubcategories)
+                .WithRequired(e => e.ProductCategory)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ProductCostHistory>()
+                .Property(e => e.StandardCost)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<ProductDescription>()
+                .HasMany(e => e.ProductModelProductDescriptionCultures)
+                .WithRequired(e => e.ProductDescription)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ProductListPriceHistory>()
+                .Property(e => e.ListPrice)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<ProductModel>()
+                .HasMany(e => e.ProductModelIllustrations)
+                .WithRequired(e => e.ProductModel)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ProductModel>()
+                .HasMany(e => e.ProductModelProductDescriptionCultures)
+                .WithRequired(e => e.ProductModel)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ProductModelProductDescriptionCulture>()
+                .Property(e => e.CultureID)
+                .IsFixedLength();
+
+            modelBuilder.Entity<ProductPhoto>()
+                .HasMany(e => e.ProductProductPhotoes)
+                .WithRequired(e => e.ProductPhoto)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<TransactionHistory>()
+                .Property(e => e.TransactionType)
+                .IsFixedLength();
+
+            modelBuilder.Entity<TransactionHistory>()
+                .Property(e => e.ActualCost)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<TransactionHistoryArchive>()
+                .Property(e => e.TransactionType)
+                .IsFixedLength();
+
+            modelBuilder.Entity<TransactionHistoryArchive>()
+                .Property(e => e.ActualCost)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<UnitMeasure>()
+                .Property(e => e.UnitMeasureCode)
+                .IsFixedLength();
+
+            modelBuilder.Entity<UnitMeasure>()
+                .HasMany(e => e.BillOfMaterials)
+                .WithRequired(e => e.UnitMeasure)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<UnitMeasure>()
+                .HasMany(e => e.Products)
+                .WithOptional(e => e.UnitMeasure)
+                .HasForeignKey(e => e.SizeUnitMeasureCode);
+
+            modelBuilder.Entity<UnitMeasure>()
+                .HasMany(e => e.Products1)
+                .WithOptional(e => e.UnitMeasure1)
+                .HasForeignKey(e => e.WeightUnitMeasureCode);
+
+            modelBuilder.Entity<WorkOrder>()
+                .HasMany(e => e.WorkOrderRoutings)
+                .WithRequired(e => e.WorkOrder)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<WorkOrderRouting>()
+                .Property(e => e.ActualResourceHrs)
+                .HasPrecision(9, 4);
+
+            modelBuilder.Entity<WorkOrderRouting>()
+                .Property(e => e.PlannedCost)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<WorkOrderRouting>()
+                .Property(e => e.ActualCost)
+                .HasPrecision(19, 4);
+        }
     }
 }
