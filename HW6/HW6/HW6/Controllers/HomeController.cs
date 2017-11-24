8773bf17b1f9ad69160d499b4b483100a9b4ecee
RahevinSlade <rpotterclark15@mail.wou.edu>
2017-11-24 11:49:28 -0800

    got the models finished, i think...

--- HW6/HW6/HW6/Controllers/HomeController.cs
+++ HW6/HW6/HW6/Controllers/HomeController.cs
@@ -18,18 +18,5 @@ namespace HW6.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
---
+++ HW6/HW6/HW6/Models/ProductReview.cs
@@ -0,0 +1,40 @@
+﻿namespace HW6.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+    using System.Web.Mvc;
+
+    [Table("Production.ProductReview")]
+    public partial class ProductReview
+    {
+        public int ProductReviewID { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public int ProductID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Name")]
+        public string ReviewerName { get; set; }
+
+        public DateTime ReviewDate { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Email")]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Rating: 1-5")]
+        public int Rating { get; set; }
+
+        [StringLength(3850)]
+        public string Comments { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public virtual Product Product { get; set; }
+    }
+}
