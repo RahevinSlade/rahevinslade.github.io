488aef7fa189001dd92144e4dcdd265826089f76
RahevinSlade <rpotterclark15@mail.wou.edu>
2017-12-05 16:40:56 -0800

    working on Ajax, going to deploy to Azure

--- Final/Final/Final/Controllers/BidsController.cs
+++ Final/Final/Final/Controllers/BidsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Final.Models;
+using System.Diagnostics;
 
 namespace Final.Controllers
 {
@@ -53,6 +54,7 @@ namespace Final.Controllers
         {
             if (ModelState.IsValid)
             {
+                bid.Stamp = DateTime.Now;
                 db.Bids.Add(bid);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -63,67 +65,6 @@ namespace Final.Controllers
             return View(bid);
         }
 
-        // GET: Bids/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Bid bid = db.Bids.Find(id);
-            if (bid == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.BuyerID = new SelectList(db.Buyers, "BuyerID", "Buyername", bid.BuyerID);
-            ViewBag.ItemID = new SelectList(db.Items, "ItemID", "ItemName", bid.ItemID);
-            return View(bid);
-        }
-
-        // POST: Bids/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "BidID,ItemID,BuyerID,Price,Stamp")] Bid bid)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(bid).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.BuyerID = new SelectList(db.Buyers, "BuyerID", "Buyername", bid.BuyerID);
-            ViewBag.ItemID = new SelectList(db.Items, "ItemID", "ItemName", bid.ItemID);
-            return View(bid);
-        }
-
-        // GET: Bids/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Bid bid = db.Bids.Find(id);
-            if (bid == null)
-            {
-                return HttpNotFound();
-            }
-            return View(bid);
-        }
-
-        // POST: Bids/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Bid bid = db.Bids.Find(id);
-            db.Bids.Remove(bid);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -132,5 +73,27 @@ namespace Final.Controllers
             }
             base.Dispose(disposing);
         }
+
+        //public JsonResult Item(int id)
+        //{
+        //    var bids = db.Bids.Find(id);
+        //    string[] currentbids = new string[bids];
+
+
+        //    //var artwork = db.Genres.Find(id).Classifications.ToList().OrderBy(t => t.ArtWork.Title).Select(a => new { aw = a.ArtWorkID, awa = a.ArtWork.ArtistID }).ToList();
+        //    //string[] artworkCreator = new string[artwork.Count()];
+        //    //for (int i = 0; i < artworkCreator.Length; ++i)
+        //    //{
+        //    //    artworkCreator[i] = $"<ul>{db.ArtWorks.Find(artwork[i].aw).Title} by {db.Artists.Find(artwork[i].awa).ArtistName}</ul>";
+        //    //}
+        //    //var data = new
+        //    //{
+        //    //    arr = artworkCreator
+        //    //};
+
+        //    //return Json(data, JsonRequestBehavior.AllowGet);
+        //    return Json(data, JsonRequestBehavior.AllowGet);
+        //}
+
     }
 }
---
+++ Final/Final/Final/Scripts/Ajax.js
@@ -0,0 +1,33 @@
+﻿var ajax_call = function (id) {
+    //your jQuery ajax code
+    var source = "/Bids/Item" + id;
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        data: { id: id },
+        url: source,
+        success: displayResults,
+        error: errorOnAjax
+    });
+};
+
+var interval = 1000 * 5; // where X is your timer interval in X seconds
+
+function displayResults(data) {//this display well the results from our search through artist with certain genre
+    console.log("AJAX Success!");
+    console.log(data);
+    $('#results').empty();
+
+    var item = document.getElementById("results");
+
+    data.arr.forEach(function (item) {
+        $('#results').append(item);
+    });
+
+}
+
+function errorOnAjax() {
+    console.log("Error");
+}
+
+window.setInterval(ajax_call, interval);
